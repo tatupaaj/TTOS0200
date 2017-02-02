a5cdd02ea9b353c991bee944db0f43e62b0b4364
Tatu <Tatu@P52>
2017-02-02 10:35:14 +0200

    commit

---
+++ Tehtävät 8/Noppa.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using JAMK.IT;
+
+namespace JAMK.IT
+{
+    class Noppa
+    {
+        public Random rnd = new Random();
+
+        float noppaluku;
+        public float NoppaLuku
+        {
+            get
+            {
+                noppaluku = rnd.Next(1, 7);
+                return noppaluku;
+            }
+            set
+            { }
+        }
+    }
+}
--- Tehtävät 8/Program.cs
+++ Tehtävät 8/Program.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Diagnostics;
+using JAMK.IT;
 
 namespace Tehtävät_8
 {
@@ -16,18 +18,52 @@ namespace Tehtävät_8
         //tehtävä 1
         static void Noppa()
         {
-            Random r = new Random();
-
-            int sum = 0;
-
-            for (int i = 0; i < 1; i++)
+            var sw = new Stopwatch();
+            List<float> Numerolista = new List<float>();
+            Noppa noppa = new Noppa();
+            Console.Write("Kuinka monta kertaa haluat heittää noppaa?: ");
+            string Valinta = Console.ReadLine();
+            double OnNumero;
+            double LaskeNumerot = 0;
+            bool OnkoNumero;
+            while (true)
             {
-                var roll = r.Next(1, 7);
-                sum += roll;
+                if (OnkoNumero = double.TryParse(Valinta, out OnNumero))
+                {
+                    break;
+                }
+                else { Console.WriteLine("Et valinnut numeroa, anna numero : "); }
             }
-            Console.WriteLine("Noppa, testi heitto: {0}\n", sum);
+            sw.Start();
+            for (int i = 0; i < OnNumero; i++)
+            {
+                Numerolista.Add(noppa.NoppaLuku);
+            }
+            for (int i = 0; i < Numerolista.Count; i++)
+            {
+                LaskeNumerot = LaskeNumerot + Numerolista[i];
+            }
+            double KeskiArvo = LaskeNumerot / Numerolista.Count;
+            Console.WriteLine("Noppaa on heitetty {0} kertaa, keskiarvo on: {1}", OnNumero, KeskiArvo);
+            Numerolista.Sort();
             
 
+            var dictionary = new Dictionary<int, int>();
+            int numero = 0;
+            for (int i = 0; i < Numerolista.Count; i++)
+            {
+                numero = Convert.ToInt32(Math.Ceiling(Numerolista[i]));
+                dictionary.Add(i, numero);
+            }
+            Dictionary<int, int> EritteleNumerot = new Dictionary<int, int>();
+            foreach (int i in dictionary.Values)
+                if (EritteleNumerot.ContainsKey(i))
+                    EritteleNumerot[i]++;
+                else
+                    EritteleNumerot[i] = 1;
+            foreach (KeyValuePair<int, int> kvp in EritteleNumerot)
+                Console.WriteLine("- Numeroa {0} on heitetty: {1}", kvp.Key, kvp.Value);
         }
+
     }
 }
